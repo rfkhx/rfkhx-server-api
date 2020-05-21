9f935e59bc6b48e3acef79332e9a686a62cd1763
归零幻想 <yqmailsend@163.com>
2020-05-21 16:50:01 +0800

    init

---
+++ src/main/java/edu/upc/mishuserverapi/MishuserverapiApplication.java
@@ -0,0 +1,13 @@
+package edu.upc.mishuserverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MishuserverapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MishuserverapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/upc/mishuserverapi/MishuserverapiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.upc.mishuserverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MishuserverapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
