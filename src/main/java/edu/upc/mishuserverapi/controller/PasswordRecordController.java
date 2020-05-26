1163a23c5906af88ff176312d43d8e7d65120d85
归零幻想 <yqmailsend@163.com>
2020-05-26 23:12:41 +0800

    修改两个借口

--- src/main/java/edu/upc/mishuserverapi/controller/PasswordRecordController.java
+++ src/main/java/edu/upc/mishuserverapi/controller/PasswordRecordController.java
@@ -1,16 +1,11 @@
 package edu.upc.mishuserverapi.controller;
 
+import java.security.Principal;
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
-
-import com.sun.tools.sjavac.Log;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +18,7 @@ import edu.upc.mishuserverapi.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/api/user/{uid}/records")
+@RequestMapping("/records")
 @Slf4j
 public class PasswordRecordController {
     @Autowired
@@ -33,25 +28,16 @@ public class PasswordRecordController {
     private PasswordRecordRepository passwordRecordRepository;
 
     @GetMapping
-    List<PasswordRecord> getAll(@PathVariable Long uid){
+    List<PasswordRecord> getAll(Principal principal){
         log.info("GET");
-        Optional<User> user=userRepository.findById(uid);
-        if(user.isEmpty()){
-            log.warn("未找到用户{}",uid);
-            return new ArrayList<>();
-        }
-        return passwordRecordRepository.findByUser(user.get());
+        User user = userRepository.findByEmail(principal.getName());
+        return passwordRecordRepository.findByUser(user);
     }
 
     @PostMapping
-    PasswordRecord create(@PathVariable Long uid,PasswordRecordDto passwordRecordDto){
-        Optional<User> user=userRepository.findById(uid);
-        log.info("提交的请求：{}",passwordRecordDto);
-        if(user.isEmpty()){
-            log.warn("未找到用户{}",uid);
-            return null;
-        }
-        PasswordRecord passwordRecord=passwordRecordRepository.findByUserAndNameAndUsername(user.get(), passwordRecordDto.getName(), passwordRecordDto.getUsername());
+    PasswordRecord create(PasswordRecordDto passwordRecordDto,Principal principal){
+        User user=userRepository.findByEmail(principal.getName());
+        PasswordRecord passwordRecord=passwordRecordRepository.findByUserAndNameAndUsername(user, passwordRecordDto.getName(), passwordRecordDto.getUsername());
         if(passwordRecord==null){
             passwordRecord=new PasswordRecord();
         }
@@ -61,7 +47,7 @@ public class PasswordRecordController {
         passwordRecord.setSynctimestamp(new Timestamp(System.currentTimeMillis()));
         passwordRecord.setType(passwordRecordDto.getType());
         passwordRecord.setUrl(passwordRecordDto.getUrl());
-        passwordRecord.setUser(user.get());
+        passwordRecord.setUser(user);
         passwordRecord.setUsername(passwordRecordDto.getUsername());
         return passwordRecordRepository.saveAndFlush(passwordRecord);
     }
